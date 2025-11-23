837244d1631e61741d759d77b0027385a519c277
Snaha Kundu <snahakundu19@gmail.com>
2025-11-23 16:35:17 +0530

    Add loop examples in loops.py
    Added examples of for and while loops in Python.

--- python/Basics/loops.py
+++ python/Basics/loops.py
@@ -1,1 +1,20 @@
+# Loops in Python
+
+# For loop
+for i in range(1, 6):
+    print("For Loop:", i)
+else:
+  <statement>
+#   An optional else part is executed if control exist the loop without the break statement
+
+# # While loop
+x = 1
+while x <= 5:
+    print("While Loop:", x)
+    x += 1
+
+## here for loop is use as a iterator,works for  STRING,LIST,TUPPLE AND OTHER BUILT IN ITERATER 
+## use while loop for general purpose
+##  range() ---> sequence generator 
+# range(start,stop,step) here start and step are optional and default value of step=1 and start=0
 
