6a6560db9dc8a03750c7148eacb6223dfe31b78f
Snaha Kundu <snahakundu19@gmail.com>
2025-11-23 16:16:21 +0530

    Add variable examples in variables.py

--- python/Basics/variables.py
+++ python/Basics/variables.py
@@ -1,1 +1,12 @@
+# Variable examples
+
+name = "Snaha"
+age = 22
+score = 89.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Score:", score)
+print("Student:", is_student)
 
