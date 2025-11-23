53d87571bd8cfa91a3b629b44cdaf5d3a6df7c9a
Snaha Kundu <snahakundu19@gmail.com>
2025-11-23 17:04:08 +0530

    Add temperature conversion program
    Implement temperature conversion from Fahrenheit to Celsius.

---
+++ python/assignments/Assignment-1/TemparatureCalculation.py
@@ -0,0 +1,28 @@
+
+# The Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a program to
+# convert this temperature into Centigrade degrees.
+
+#taking input of the temp of any city in farenheit degree
+
+degree_f=float(input("enter temp in farenheit degree :"))
+
+
+# calculating temp in celcius
+
+degree_c= (degree_f-32)*9/5
+
+
+# printing output
+print(f"{degree_f} degree farenheight =  {degree_c} centegrade degree")
+
+
+## output:
+enter temp in farenheit degree : 45
+45.0 degree farenheight =  23.4 centegrade degree
+
+
+
+
+
+
+
