f3eed3b7e2d39a33ce405bee181bcc352672c533
Snaha Kundu <snahakundu19@gmail.com>
2025-11-23 16:39:18 +0530

    Implement greet and add functions in functions.py
    Added functions to greet a user and add two numbers.

--- python/Basics/functions.py
+++ python/Basics/functions.py
@@ -1,1 +1,16 @@
+# Functions in Python
 
+def greet(name):
+    return "Hello " + name
+
+def add(a, b):
+    return a + b
+
+print(greet("Snaha"))
+print("Sum:", add(10, 20))
+
+
+1.func is a built in module and its user define
+2.it execute in runtime
+3.it can be assigned to diff name and can be call through the new name
+4.in python every thing is called as obj,fuction allow arbitary attribute to be attached to record info for latter use
